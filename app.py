4769dbcaf34b7f1eb648c3f32178e0a96e76e770
Danil <kidvsking@yahoo.com>
2015-08-15 23:39:04 -0400

    d3
    started implementing basic interaction in the front end

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import render_template
 import json
+import time
+import random
 
-#import pyorient
+import pyorient
 
 app = Flask(__name__)
 
@@ -11,43 +13,48 @@ def index():
     return render_template("index.html")
 
 @app.route("/listings")
-def donorschoose_projects():
+def getListings():
 
-	# #ORIENTDB IMPLEMENTATION
-	# client = pyorient.OrientDB("localhost", 2424)
-	# session_id = client.connect("root", "password")
+	#ORIENTDB IMPLEMENTATION
+	client = pyorient.OrientDB("localhost", 2424)
+	session_id = client.connect("root", "admin")
 
-	# db_name = "property_test"
+	db_name = "soufun"
 
-	# if client.db_exists( db_name, pyorient.STORAGE_TYPE_MEMORY ):
-	# 	client.db_open( db_name, "admin", "admin" )
-	# else:
-	# 	print "database does not exist!"
-	# 	sys.exit()
+	if client.db_exists( db_name, pyorient.STORAGE_TYPE_MEMORY ):
+		client.db_open( db_name, "admin", "admin" )
+	else:
+		print "database does not exist!"
+		sys.exit()
 
-	# recordsDict = {"type":"FeatureCollection","features":[]}
-	# records = client.command('SELECT FROM Listing WHERE [latitude,longitude,$spatial] NEAR [41.177407, 80.290192, {"maxDistance": 1}]')
+	recordsDict = {"type":"FeatureCollection","features":[]}
 
-	# for record in records:
-	# 	recordDict = {"type":"Feature","properties":{},"geometry":{"type":"Point"}}
-	# 	recordDict["id"] = record._rid
-	# 	recordDict["properties"]["name"] = record.title
-	# 	recordDict["properties"]["price"] = record.price
-	# 	recordDict["geometry"]["coordinates"] = [record.longitude, record.latitude]
+	records = client.command('SELECT FROM Listing WHERE [latitude,longitude,$spatial] NEAR [41.177407, 80.290192, {"maxDistance": 15}] ORDER BY RAND() LIMIT 100')
 
-	# 	recordsDict["features"].append(recordDict)
+	random.shuffle(records)
+	records = records[:15]
 
-	# client.db_close()
+	for record in records:
+		recordDict = {"type":"Feature","properties":{},"geometry":{"type":"Point"}}
+		recordDict["id"] = record._rid
+		recordDict["properties"]["name"] = record.title
+		recordDict["properties"]["price"] = record.price
+		recordDict["geometry"]["coordinates"] = [record.longitude, record.latitude]
+
+		recordsDict["features"].append(recordDict)
+
+	client.db_close()
 
 
 	#DUMMY DATA IMPLEMENTATION
-	with open("static/data.txt", 'r') as f:
-		recordsDict = json.loads(f.read())
+	# with open("static/data.txt", 'r') as f:
+	# 	recordsDict = json.loads(f.read())
 
 	print "acquired!"
 
 	#pass GeoJSON data back to D3
 	return json.dumps(recordsDict)
+
 
 
 if __name__ == "__main__":
