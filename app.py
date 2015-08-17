7d68704c122e17743b64bbcbafadbcef5e9befbe
danilnagy <kidvsking@yahoo.com>
2015-08-17 11:00:04 -0400

    update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template
 import json
 
-#import pyorient
+import pyorient
 
 app = Flask(__name__)
 
@@ -11,38 +11,38 @@ def index():
     return render_template("index.html")
 
 @app.route("/listings")
-def donorschoose_projects():
+def getData():
 
-	# #ORIENTDB IMPLEMENTATION
-	# client = pyorient.OrientDB("localhost", 2424)
-	# session_id = client.connect("root", "password")
+	#ORIENTDB IMPLEMENTATION
+	client = pyorient.OrientDB("localhost", 2424)
+	session_id = client.connect("root", "password")
 
-	# db_name = "property_test"
+	db_name = "property_test"
 
-	# if client.db_exists( db_name, pyorient.STORAGE_TYPE_MEMORY ):
-	# 	client.db_open( db_name, "admin", "admin" )
-	# else:
-	# 	print "database does not exist!"
-	# 	sys.exit()
+	if client.db_exists( db_name, pyorient.STORAGE_TYPE_MEMORY ):
+		client.db_open( db_name, "admin", "admin" )
+	else:
+		print "database does not exist!"
+		sys.exit()
 
-	# recordsDict = {"type":"FeatureCollection","features":[]}
-	# records = client.command('SELECT FROM Listing WHERE [latitude,longitude,$spatial] NEAR [41.177407, 80.290192, {"maxDistance": 1}]')
+	recordsDict = {"type":"FeatureCollection","features":[]}
+	records = client.command('SELECT FROM Listing WHERE [latitude,longitude,$spatial] NEAR [41.177407, 80.290192, {"maxDistance": 2}]')
 
-	# for record in records:
-	# 	recordDict = {"type":"Feature","properties":{},"geometry":{"type":"Point"}}
-	# 	recordDict["id"] = record._rid
-	# 	recordDict["properties"]["name"] = record.title
-	# 	recordDict["properties"]["price"] = record.price
-	# 	recordDict["geometry"]["coordinates"] = [record.longitude, record.latitude]
+	for record in records:
+		recordDict = {"type":"Feature","properties":{},"geometry":{"type":"Point"}}
+		recordDict["id"] = record._rid
+		recordDict["properties"]["name"] = record.title
+		recordDict["properties"]["price"] = record.price
+		recordDict["geometry"]["coordinates"] = [record.longitude, record.latitude]
 
-	# 	recordsDict["features"].append(recordDict)
+		recordsDict["features"].append(recordDict)
 
-	# client.db_close()
+	client.db_close()
 
 
-	#DUMMY DATA IMPLEMENTATION
-	with open("static/data.txt", 'r') as f:
-		recordsDict = json.loads(f.read())
+	# #DUMMY DATA IMPLEMENTATION
+	# with open("static/data.txt", 'r') as f:
+	# 	recordsDict = json.loads(f.read())
 
 	print "acquired!"
 
